fa06afd3608ab58de104f5bdfd1e38bd1e09898e
mkeymolen <keymolen.michael@gmail.com>
2017-06-07 11:39:47 +0200

    it is now possible to get the customfields with list values.

---
+++ src/TeamleaderDotNet/CustomFields/CustomFieldInfo.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TeamleaderDotNet.Crm;
+
+namespace TeamleaderDotNet.CustomFields
+{
+    public class CustomFieldInfo : Entity
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "for")]
+        public string ForObjectType { get; set; }
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "options")]
+        public JObject Options { get; set; }
+    }
+}
--- src/TeamleaderDotNet/TeamleaderCustomFieldsApi.cs
+++ src/TeamleaderDotNet/TeamleaderCustomFieldsApi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using TeamleaderDotNet.Common;
 using TeamleaderDotNet.Crm;
@@ -26,10 +27,28 @@ namespace TeamleaderDotNet
             return await DoCall<List<CustomField>>("getCustomFields.php", new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>("for", objectType)
-            });  
+            });
         }
-
-
+        /// <summary>
+        /// Getting the extra info of a custom field from cache or via api call to Teamleader
+        /// </summary>
+        /// <param name="customFieldId">The CustomField ID</param>
+        /// <returns></returns>
+        public async Task<CustomFieldInfo> GetCustomFieldInfo(int customFieldId)
+        {
+            var cacheKey = "customfield_" + customFieldId;
+            var cache = MemoryCache.Default;
+            var customFieldInfo = cache[cacheKey] as CustomFieldInfo;
+            if (customFieldInfo == null)
+            {
+                customFieldInfo = await DoCall<CustomFieldInfo>("getCustomFieldInfo.php", new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("custom_field_id", customFieldId.ToString())
+                });
+                cache.Add(cacheKey, customFieldInfo, new CacheItemPolicy(){SlidingExpiration = TimeSpan.FromHours(1)});
+            }
+            return customFieldInfo;
+        }
 
     }
 }
