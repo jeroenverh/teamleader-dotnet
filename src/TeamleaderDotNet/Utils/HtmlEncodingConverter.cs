766b7d150e68bae921a39aa4135a0ec197e6117c
mkeymolen <keymolen.michael@gmail.com>
2017-12-11 10:55:50 +0100

    Added HtmlEncoder during deserialization

---
+++ src/TeamleaderDotNet/Utils/HtmlEncodingConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TeamleaderDotNet.Utils
+{
+    public class HtmlEncodingConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(String);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return WebUtility.HtmlDecode((string)reader.Value);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(WebUtility.HtmlEncode((string)value));
+        }
+    }
+}
